26d01a4d0650be3597a046dc901516cda0f467cc
Niklas Karlsson <nafrali@gmail.com>
2016-04-11 13:29:10 +0200

    addade db files

---
+++ dbfile.h
@@ -0,0 +1,40 @@
+#ifndef DBFILE_H
+#define DBFILE_H
+
+#include "database.h"
+
+
+class NewsFile : public News {
+	public:
+		NewsFile(std::string folder, int id, int inodeNbr) : folder(folder), id(id)  {
+		nextId = 0;
+		}
+		~NewsFile() {};
+		std::string getTitle() { return folder; }
+		int getId() { return id; }
+		void createArticle(std::string title, std::string author, std::string text);
+		bool deleteArticle(int id); 
+		std::vector<Article*> getArticles();
+		Article* getArticle(int id);
+	private:
+		unsigned long findNextId();
+		std::string folder;
+		int id;
+		int inodeNbr;
+		unsigned int nextId;
+};
+
+class DBFile : public Database {
+	public:
+		DBFile();
+		std::vector<News*> getNewsgroups();
+		bool createNewsgroup(std::string title);
+		bool deleteNewsgroup(int id);
+		News* getNewsgroup(int id);
+		News* getNewsgroup(std::string title);
+	private:
+		unsigned int inodeNbr;
+		unsigned int nextId;
+};
+
+#endif
