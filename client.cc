1ec2a9da21813c96efbbc2c883e92716dd485951
Niklas Karlsson <nafrali@gmail.com>
2016-04-18 16:10:45 +0200

    Added manual to the client

--- client.cc
+++ client.cc
@@ -8,6 +8,19 @@ bool executeCommand(std::istream& input, std::shared_ptr<Connection>, int &grpNb
 int findNumber(const std::shared_ptr<Connection>& con);
 std::string findString(const std::shared_ptr<Connection>& con);
 
+void printManual() {
+	std::cout << std::endl;
+	std::cout << "Type 1 to list the different newsgroups" << std::endl;	
+	std::cout << "Type 2 <groupname> to create a new newsgroup" << std::endl;	
+	std::cout << "Type 3 <group id>  to delete a  newsgroup" << std::endl;	
+	std::cout << "Type 4 <group id> to list the articles in a group" << std::endl;	
+	std::cout << "Type 5 <group id>  <Title> <enter> Author <enter> <text> to create an article" << std::endl;	
+	std::cout << "Type 6 <group id> <article id> to delete an article" << std::endl;	
+	std::cout << "Type 7 <groups id> <article id> to read an article from a group" << std::endl;	
+	std::cout << "Type q to close the program" << std::endl;	
+}
+
+
 int main(int argc, char* argv[]) {
 
 	if (argc != 3) {
@@ -32,6 +45,7 @@ int main(int argc, char* argv[]) {
 	std::cout << "Connected to server" << std::endl;
 	std::string input = "";
 	int grpNbr = -1;
+	printManual();
 	while(true) {
 		executeCommand(std::cin, con, grpNbr);
 		input = "";
@@ -42,10 +56,12 @@ int main(int argc, char* argv[]) {
 }
 
 
+
+
 bool executeCommand(std::istream& input, std::shared_ptr<Connection> con, int &grpNbr) {
 	MessageHandler message;
 	std::string command = "";
-	getline(input, command);
+	input >> command;
 	//	input >> command;
 	std::cout << std::endl;
 	unsigned char response;
@@ -53,13 +69,14 @@ bool executeCommand(std::istream& input, std::shared_ptr<Connection> con, int &g
 	std::string text;
 	std::string title = ""; //To be used for various commands
 	int number = -1; //To be used for various commands
-	if(command == "list-groups") {
+	if(command == "1") {
 		message.sendChar((unsigned char) Protocol::COM_LIST_NG, con);
 		message.sendChar((unsigned char) Protocol::COM_END, con);
 		
 		con->read();
 		con->read();
 		int nbrOfGroups = findNumber(con);
+		
 		std::string name = "";
 		for (int i = 0; i != nbrOfGroups; ++i) {
 			con->read();
@@ -70,13 +87,53 @@ bool executeCommand(std::istream& input, std::shared_ptr<Connection> con, int &g
 		}
 	con->read();
 	}
-	else if (command == "use") {
-		input >> grpNbr;
+	else if (command == "2") {
+		std::getline(input, title);
+		message.sendChar((unsigned char) Protocol::COM_CREATE_NG, con);
+		message.sendChar((unsigned char)Protocol::PAR_STRING, con);
+		message.sendInt(title.size(), con); 
+		message.sendString(title, con); 
+		message.sendChar((unsigned char) Protocol::COM_END, con);
+
+
+		con->read();	
+
+
+		response = con->read();
+		
+
+		if(response == Protocol::ANS_ACK){
+			std::cout << "The newsgroup named " << title << " has been created." << std::endl;
+		} else{
+			std::cout << "The newsgroup named " << title << " has NOT been created." << std::endl;
+			response = con->read();
+		}
+		con->read();
 	}
-	else if (command == "list-articles") {
+	else if (command == "3") {
+		input >> number;
+		message.sendChar((unsigned int) Protocol::COM_DELETE_NG, con);
+		message.sendChar((unsigned int) Protocol::PAR_NUM, con);
+		message.sendInt(number, con);
+		message.sendChar((unsigned int) Protocol::COM_END, con);
+
+		con->read();
+		response = con->read();
+		if (response == Protocol::ANS_ACK)
+			std::cout << "Newsgroup removed" << std::endl;
+		else {
+			std::cout << "Failed to remove newsgroup" << std::endl;
+		}
+		
+
+	con->read();
+	}
+
+	else if (command == "4") {
+		input >> number;
 		message.sendChar((unsigned char) Protocol::COM_LIST_ART, con);
 		message.sendChar((unsigned char) Protocol::PAR_NUM, con);
-		message.sendInt(grpNbr, con);
+		message.sendInt(number, con);
 		message.sendChar((unsigned char) Protocol::COM_END, con);
 
 		con->read();
@@ -100,71 +157,19 @@ bool executeCommand(std::istream& input, std::shared_ptr<Connection> con, int &g
 
 
 	}
-	else if (command == "read") {
+	else if (command == "5") {
 		input >> number;
-		std::cout << "Reading article number: " << number << std::endl;
-		message.sendChar((unsigned char) Protocol::COM_GET_ART, con);
-		message.sendChar((unsigned char) Protocol::PAR_NUM, con);
-		message.sendInt(grpNbr, con);
-		message.sendChar((unsigned char) Protocol::PAR_NUM, con);
-		message.sendInt(number, con);
-		message.sendChar((unsigned char) Protocol::COM_END, con);
-	
-		//read input
-		//
-		con->read();
-		unsigned char ack = con->read();
-		if (ack == Protocol::ANS_ACK) {
-			std::cout << "Article found!" << std::endl;
-			ack = con->read();
-			while(ack == Protocol::PAR_STRING) {
-				std::string text = findString(con);
-				std::cout << text << std::endl;
-				ack = con->read();
-			}
-			ack = con->read();
-		}
-		else {
-			std::cout << "Failed to read article" << std::endl;
-			con->read();
-		}
-		con->read();
-	}
-	else if (command == "createng") {
 		std::getline(input, title);
-		std::cout << "titel: " << title << std::endl;
-		message.sendChar((unsigned char) Protocol::COM_CREATE_NG, con);
-		message.sendChar((unsigned char)Protocol::PAR_STRING, con);
-		message.sendInt(title.size(), con); 
-		message.sendString(title, con); 
-		message.sendChar((unsigned char) Protocol::COM_END, con);
-
-
-		con->read();	
-
-		std::cout << "innan read response " << std::endl;
-
-		response = con->read();
-		std::cout << "response:  " << response << std::endl;
-		
-
-		if(response == Protocol::ANS_ACK){
-			std::cout << "The newsgroup named " << title << " has been created." << std::endl;
-		} else{
-			std::cout << "The newsgroup named " << title << " has NOT been created." << std::endl;
-			response = con->read();
-		}
-		con->read();
-	}
-		else if (command == "createart") {
-		std::getline(input, title);
+		std::cout << title << std::endl;
 		std::getline(input, auhtor);
+		std::cout << auhtor << std::endl;
 		std::getline(input, text);
+		std::cout << text << std::endl;
 
 		message.sendChar((unsigned char) Protocol::COM_CREATE_ART, con);
 
 		message.sendChar((unsigned char)Protocol::PAR_NUM, con);
-		message.sendInt(grpNbr, con); 
+		message.sendInt(number, con); 
 
 		message.sendChar((unsigned char)Protocol::PAR_STRING, con);
 		message.sendInt(title.size(), con); 
@@ -183,10 +188,8 @@ bool executeCommand(std::istream& input, std::shared_ptr<Connection> con, int &g
 
 		con->read();	
 
-		std::cout << "innan read response " << std::endl;
 
 		response = con->read();
-		std::cout << "response:  " << response << std::endl;
 		
 
 		if(response == Protocol::ANS_ACK){
@@ -197,25 +200,66 @@ bool executeCommand(std::istream& input, std::shared_ptr<Connection> con, int &g
 		}
 		response = con->read();
 	}
-	else if (command == "delete-ng") {
+	else if (command == "6") {
+		int group;
+		input >> group;
 		input >> number;
-		message.sendChar((unsigned int) Protocol::COM_DELETE_NG, con);
+		message.sendChar((unsigned int) Protocol::COM_DELETE_ART, con);
+		message.sendChar((unsigned int) Protocol::PAR_NUM, con);
+		message.sendInt(group, con);
 		message.sendChar((unsigned int) Protocol::PAR_NUM, con);
 		message.sendInt(number, con);
 		message.sendChar((unsigned int) Protocol::COM_END, con);
-
+		
 		con->read();
 		response = con->read();
-		if (response == Protocol::ANS_ACK)
-			std::cout << "Newsgroup removed" << std::endl;
-		else
-			std::cout << "Failed to remove newsgroup" << std::endl;
-		
+		if (response == Protocol::ANS_ACK) {
+			std::cout << "Successfully deleted the article" << std::endl;
+		}
+		else {
+			std::cout << "Failed to delete the article" << std::endl;
+			con->read();
+		}
+		con->read();
+	}
+//Nogt er fucked
+	else if (command == "7") {
+		int group;
+		input >> group;
+		input >> number;
+		std::cout << "Reading article number: " << number << std::endl;
+		message.sendChar((unsigned char) Protocol::COM_GET_ART, con);
+		message.sendChar((unsigned char) Protocol::PAR_NUM, con);
+		message.sendInt(group, con);
+		message.sendChar((unsigned char) Protocol::PAR_NUM, con);
+		message.sendInt(number, con);
+		message.sendChar((unsigned char) Protocol::COM_END, con);
 
-	con->read();
+		con->read();
+		unsigned char ack = con->read();
+		if (ack == Protocol::ANS_ACK) {
+			std::cout << "Title: ";
+			con->read();
+			text = findString(con);
+			std::cout << text << std::endl;
+			con->read();
+			text = findString(con);
+			std::cout << "Author: " << text << std::endl;
+			con->read();
+			text = findString(con);
+			std::cout << std::endl << text << std::endl;
+		}
+		else {
+			con->read();
+			std::cout << "Failed to fetch article" << std::endl;
+		}
+		con->read();
+	}
+	else if (command == "q") {
+		exit(0);
 	}
 	else
-		std::cerr << "Unknown command" << std::endl;
+	printManual();
 	
 	return false;
 }
